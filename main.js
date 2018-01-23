de052fddb1836cc632340725b9b195fe31adf2e3
Stephanie D <stephanie_day8@msn.com>
2018-01-23 13:23:28 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,99 @@
+function Animation(spriteSheet, startX, startY, frameWidth, frameHeight, sheetWidth, frameDuration, frames, loop) {
+    this.spriteSheet = spriteSheet;
+    this.startX = startX * frameWidth;
+    this.startY = startY * frameHeight;
+    this.frameWidth = frameWidth;
+    this.sheetWidth = sheetWidth;
+    this.frameDuration = frameDuration;
+    this.frameHeight = frameHeight;
+    this.frames = frames;
+    this.totalTime = frameDuration * frames;
+    this.elapsedTime = 0;
+    this.loop = loop;
+}
+
+Animation.prototype.drawFrame = function (tick, ctx, x, y) {
+    this.elapsedTime += tick;
+    if (this.loop) {
+        if (this.isDone()) {
+            this.elapsedTime = 0;
+        }
+    } else if (this.isDone()) {
+        return;
+    }
+
+    var colIndex = this.currentFrame() % this.sheetWidth
+    var rowIndex = Math.floor(this.currentFrame() / this.sheetWidth);
+
+    if ((colIndex + 1) * this.frameWidth > this.spriteSheet.width) {
+        rowIndex++;
+    }
+  
+    ctx.drawImage(this.spriteSheet, colIndex * this.frameWidth + this.startX,
+        rowIndex * this.frameHeight + this.startY, this.frameWidth,
+        this.frameHeight, x, y, this.frameWidth, this.frameHeight);
+}
+
+Animation.prototype.currentFrame = function () {
+    return Math.floor(this.elapsedTime / this.frameDuration);
+}
+
+Animation.prototype.isDone = function () {
+    return (this.elapsedTime >= this.totalTime);
+}
+
+function Background(game) {
+    Entity.call(this, game, 0, 400);
+    this.radius = 200;
+}
+
+Background.prototype = new Entity();
+Background.prototype.constructor = Background;
+
+Background.prototype.update = function () {
+}
+
+Background.prototype.draw = function (ctx) {
+    ctx.fillStyle = "SaddleBrown";
+    ctx.fillRect(0,500,800,300);
+    Entity.prototype.draw.call(this);
+}
+function Weaver(game) {
+    this.animation = new Animation(ASSET_MANAGER.getAsset("./img/Weaver.png"), 0, 1, 118, 100, 6, .15, 12, true);
+    Entity.call(this, game, 0, 400);
+
+}
+
+Weaver.prototype = new Entity();
+Weaver.prototype.constructor = Weaver;
+
+Weaver.prototype.update = function () {
+    Entity.prototype.update.call(this);
+}
+
+Weaver.prototype.draw = function (ctx) {
+    this.animation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    Entity.prototype.draw.call(this);
+}
+
+// the "main" code begins here
+
+var ASSET_MANAGER = new AssetManager();
+
+ASSET_MANAGER.queueDownload("./img/Weaver.png");
+
+ASSET_MANAGER.downloadAll(function () {
+    console.log("starting up da sheild");
+    var canvas = document.getElementById('gameWorld');
+    var ctx = canvas.getContext('2d');
+
+    var gameEngine = new GameEngine();
+    var bg = new Background(gameEngine);
+    var weaver = new Weaver(gameEngine);
+
+    gameEngine.addEntity(bg);
+    gameEngine.addEntity(weaver);
+ 
+    gameEngine.init(ctx);
+    gameEngine.start();
+});
