6902310f530354caad361a6b7583d29ee65aa9a3
Stephanie D <stephanie_Day8@msn.com>
2018-01-26 14:20:17 -0800

    Revert "Added all walkers? fix animations"
    This reverts commit cd13f7fb24669265f9d488e8753d798e455e6c3e.
    fjiaojsxa

---
+++ Walkers.js
@@ -0,0 +1,142 @@
+
+var speed = 50;
+var aSpeed = .05;
+
+function Walker4(game, img, lX, lY) {
+    this.sY = lY;
+    this.animation = null;
+    this.img = img;
+    Entity.call(this, game, lX, lY);
+}
+
+Walker4.prototype = new Entity();
+Walker4.prototype.constructor = Walker4;
+
+Walker4.prototype.update = function () {
+    this.x += this.game.clockTick * speed;
+    this.y -= this.game.clockTick * speed;
+    if (this.x > 800 || this.y < -50) {
+        this.x = -100;
+        this.y = this.sY;
+    }
+    Entity.prototype.update.call(this);
+}
+
+Walker4.prototype.draw = function (ctx) {
+    this.animation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    Entity.prototype.draw.call(this);
+}
+
+//Cart Walkers 
+
+function eCartMan(game, img, lX, lY) {
+    Walker4.call(this, game, img, lX, lY);
+    this.animation = new Animation(img, 0, 0, 60, 48, 12, aSpeed, 12, true);
+}
+
+eCartMan.prototype = new Walker4();
+eCartMan.prototype.constructor = eCartMan;
+
+function barCartMan(game, img, lX, lY) {
+    Walker4.call(this, game, img, lX, lY);
+    this.animation = new Animation(img, 0, 0, 60, 60, 12, aSpeed, 12, true);
+}
+
+barCartMan.prototype = new Walker4();
+barCartMan.prototype.constructor = barCartMan;
+
+function beCartMan(game, img, lX, lY) {
+    Walker4.call(this, game, img, lX, lY);
+    this.animation = new Animation(img, 0, 0, 60, 60, 12, aSpeed, 12, true);
+}
+
+beCartMan.prototype = new Walker4();
+beCartMan.prototype.constructor = beCartMan;
+
+function cCartMan(game, img, lX, lY) {
+    Walker4.call(this, game, img, lX, lY);
+    this.animation = new Animation(img, 0, 0, 60, 48, 12, aSpeed, 12, true);
+}
+
+cCartMan.prototype = new Walker4();
+cCartMan.prototype.constructor = cCartMan;
+
+function fCartMan(game, img, lX, lY) {
+    Walker4.call(this, game, img, lX, lY);
+    this.animation = new Animation(img, 0, 0, 60, 48, 12, aSpeed, 12, true);
+}
+
+fCartMan.prototype = new Walker4();
+fCartMan.prototype.constructor = fCartMan;
+
+function glCartMan(game, img, lX, lY) {
+    Walker4.call(this, game, img, lX, lY);
+    this.animation = new Animation(img, 0, 0, 60, 48, 12, aSpeed, 12, true);
+}
+
+glCartMan.prototype = new Walker4();
+glCartMan.prototype.constructor = glCartMan;
+
+function grCartMan(game, img, lX, lY) {
+    Walker4.call(this, game, img, lX, lY);
+    this.animation = new Animation(img, 0, 0, 60, 48, 12, aSpeed, 12, true);
+}
+
+grCartMan.prototype = new Walker4();
+grCartMan.prototype.constructor = grCartMan;
+
+function lCartMan(game, img, lX, lY) {
+    Walker4.call(this, game, img, lX, lY);
+    this.animation = new Animation(img, 0, 0, 60, 48, 12, aSpeed, 12, true);
+}
+
+lCartMan.prototype = new Walker4();
+lCartMan.prototype.constructor = lCartMan;
+
+function mCartMan(game, img, lX, lY) {
+    Walker4.call(this, game, img, lX, lY);
+    this.animation = new Animation(img, 0, 0, 60, 48, 12, aSpeed, 12, true);
+}
+
+mCartMan.prototype = new Walker4();
+mCartMan.prototype.constructor = mCartMan;
+
+function pCartMan(game, img, lX, lY) {
+    Walker4.call(this, game, img, lX, lY);
+    this.animation = new Animation(img, 0, 0, 60, 48, 12, aSpeed, 12, true);
+}
+
+pCartMan.prototype = new Walker4();
+pCartMan.prototype.constructor = pCartMan;
+
+function watahMan(game, img, lX, lY) {
+    Walker4.call(this, game, img, lX, lY);
+    this.animation = new Animation(img, 0, 0, 48, 46, 12, aSpeed, 12, true);
+}
+
+watahMan.prototype = new Walker4();
+watahMan.prototype.constructor = watahMan;
+
+//End of Cart Walkers
+
+//function Walker8(game, imgNE, imgNW, imgSE, imgSW, imgD, imgN, imgS, imgE, imgW, lX, lY) {
+//    Walker4.call(game, imgNE, imgNW, imgSE, imSW, imgD, lX, lY);
+//    this.animationN = imgN;
+//    this.animationS = imgS;
+//    this.animationE = imgE;
+//    this.animationW = imgW;
+//}
+
+//Walker8.prototype = new Walker4();
+//Walker8.prototype.constructor = Walker8;
+
+//Walker8.prototype.update = function () {
+//    //TODO Generalized Updating
+//    Walker4.prototype.update.call(this);
+//}
+
+//Walker8.prototype.draw = function () {
+//    //TODO Generalized Drawing
+//    Walker4.prototype.draw.call(this);
+//}
+
--- main.js
+++ main.js
@@ -66,20 +66,18 @@ Background.prototype.draw = function (ctx) {
 var ASSET_MANAGER = new AssetManager();
 
 ASSET_MANAGER.queueDownload("./img/Weaver.png");
-ASSET_MANAGER.queueDownload("./img/ArchBuild-1.png");
-ASSET_MANAGER.queueDownload("./img/Bazaar.png");
-ASSET_MANAGER.queueDownload("./img/COPS-1.png");
-ASSET_MANAGER.queueDownload("./img/Brewery.png");
-ASSET_MANAGER.queueDownload("./img/Firehouse-1.png");
-ASSET_MANAGER.queueDownload("./img/GoldMine.png");
-ASSET_MANAGER.queueDownload("./img/HousingAlone.png");
-ASSET_MANAGER.queueDownload("./img/HuntingLodge.png");
-ASSET_MANAGER.queueDownload("./img/Mansion.png");
-ASSET_MANAGER.queueDownload("./img/Potter.png");
-ASSET_MANAGER.queueDownload("./img/farm1.png");
-ASSET_MANAGER.queueDownload("./img/taxHouse.png");
-ASSET_MANAGER.queueDownload("./img/palace.png");
-ASSET_MANAGER.queueDownload("./img/FarmPlots.png");
+ASSET_MANAGER.queueDownload("./img/emptyCartMan.png");
+ASSET_MANAGER.queueDownload("./img/barleyCartMan.png");
+ASSET_MANAGER.queueDownload("./img/beerCartMan.png");
+ASSET_MANAGER.queueDownload("./img/beerCartMan.png");
+ASSET_MANAGER.queueDownload("./img/clayCartMan.png");
+ASSET_MANAGER.queueDownload("./img/flaxCartMan.png");
+ASSET_MANAGER.queueDownload("./img/goldCartMan.png");
+ASSET_MANAGER.queueDownload("./img/grainCartMan.png");
+ASSET_MANAGER.queueDownload("./img/linenCartMan.png");
+ASSET_MANAGER.queueDownload("./img/meatCartMan.png");
+ASSET_MANAGER.queueDownload("./img/potsCartMan.png");
+ASSET_MANAGER.queueDownload("./img/WatahMan.png");
 
 ASSET_MANAGER.downloadAll(function () {
     console.log("starting up da sheild");
@@ -88,39 +86,34 @@ ASSET_MANAGER.downloadAll(function () {
 
     var gameEngine = new GameEngine();
     var bg = new Background(gameEngine);
-    var weaver = new Weaver(gameEngine, ASSET_MANAGER.getAsset("./img/Weaver.png"));
-    var archbuild = new ArchBuild(gameEngine, ASSET_MANAGER.getAsset("./img/ArchBuild-1.png"));
-    var bazaar = new Bazaar(gameEngine, ASSET_MANAGER.getAsset("./img/Bazaar.png"));
-    var copstore = new CopStore(gameEngine, ASSET_MANAGER.getAsset("./img/COPS-1.png"));
-    var brewery = new Brewery(gameEngine, ASSET_MANAGER.getAsset("./img/Brewery.png"));
-    var firehouse = new Firehouse(gameEngine, ASSET_MANAGER.getAsset("./img/Firehouse-1.png"));
-    var goldmine = new Goldmine(gameEngine, ASSET_MANAGER.getAsset("./img/GoldMine.png"));
-    var housingalone = new Housing(gameEngine, ASSET_MANAGER.getAsset("./img/HousingAlone.png"));
-    var huntinglodge = new HuntingLodge(gameEngine, ASSET_MANAGER.getAsset("./img/HuntingLodge.png"));
-    var mansion = new Mansion(gameEngine, ASSET_MANAGER.getAsset("./img/Mansion.png"));
-    var potter = new Potter(gameEngine, ASSET_MANAGER.getAsset("./img/Potter.png"));
-    var workcamp = new WorkCamp(gameEngine, ASSET_MANAGER.getAsset("./img/farm1.png"));
-    var taxhouse = new TaxHouse(gameEngine, ASSET_MANAGER.getAsset("./img/taxHouse.png"));
-    var palace = new Palace(gameEngine, ASSET_MANAGER.getAsset("./img/palace.png"));
-    var barley = new Barley(gameEngine, ASSET_MANAGER.getAsset("./img/FarmPlots.png"));
 
+    var weaver = new Weaver(gameEngine);
+    var ecm = new eCartMan(gameEngine, ASSET_MANAGER.getAsset("./img/emptyCartMan.png"), 0, 300);
+    var barcm = new barCartMan(gameEngine, ASSET_MANAGER.getAsset("./img/barleyCartMan.png"), 0, 350);
+    var becm = new beCartMan(gameEngine, ASSET_MANAGER.getAsset("./img/beerCartMan.png"), 0, 400);
+    var ccm = new cCartMan(gameEngine, ASSET_MANAGER.getAsset("./img/clayCartMan.png"), 0, 450);
+    var fcm = new fCartMan(gameEngine, ASSET_MANAGER.getAsset("./img/flaxCartMan.png"), 0, 500);
+    var glcm = new glCartMan(gameEngine, ASSET_MANAGER.getAsset("./img/goldCartMan.png"), 0, 550);
+    var grcm = new grCartMan(gameEngine, ASSET_MANAGER.getAsset("./img/grainCartMan.png"), 0, 600);
+    var lcm = new lCartMan(gameEngine, ASSET_MANAGER.getAsset("./img/linenCartMan.png"), 0, 650);
+    var mcm = new mCartMan(gameEngine, ASSET_MANAGER.getAsset("./img/meatCartMan.png"), 0, 700);
+    var pcm = new pCartMan(gameEngine, ASSET_MANAGER.getAsset("./img/potsCartMan.png"), 0, 750);
+    var wm = new watahMan(gameEngine, ASSET_MANAGER.getAsset("./img/WatahMan.png"), 0, 780);
 
-    gameEngine.addEntity(bg);
-    gameEngine.addEntity(weaver);
-    gameEngine.addEntity(archbuild);
-    gameEngine.addEntity(bazaar);
-    gameEngine.addEntity(copstore);
-    gameEngine.addEntity(brewery);
-    gameEngine.addEntity(firehouse);
-    gameEngine.addEntity(goldmine);
-    gameEngine.addEntity(housingalone);
-    gameEngine.addEntity(huntinglodge);
-    gameEngine.addEntity(mansion);
-    gameEngine.addEntity(potter);
-    gameEngine.addEntity(workcamp);
-    gameEngine.addEntity(taxhouse);
-    gameEngine.addEntity(palace);
-    gameEngine.addEntity(barley);
+    gameEngine.addEntity(ecm);
+    gameEngine.addEntity(barcm);
+    gameEngine.addEntity(becm);
+    gameEngine.addEntity(ccm);
+    gameEngine.addEntity(fcm);
+    gameEngine.addEntity(glcm);
+    gameEngine.addEntity(grcm);
+    gameEngine.addEntity(lcm);
+    gameEngine.addEntity(mcm);
+    gameEngine.addEntity(pcm);
+    gameEngine.addEntity(wm);
+    //gameEngine.addEntity(bg);
+    //gameEngine.addEntity(weaver);
+
  
     gameEngine.init(ctx);
     gameEngine.start();
