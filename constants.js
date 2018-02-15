5515c426f155deee053cc7a040e62bcc127a9572
unknown <cynthiamora97@gmail.com>
2018-02-15 01:32:45 -0800

    contains building types and their children

---
+++ constants.js
@@ -0,0 +1,11 @@
+var Constants = {
+    Buildings : {
+        Housing: [
+            {value: "Option 1", text: "House 1"},
+            {value: "Option 2", text: "House 2"},
+            {value: "Option 3", text: "House 3"},
+        ],
+        Roads: [],
+        // TODO add all of the buildins
+    }
+}
